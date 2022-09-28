9dba176fe1b8795fb7384d2fd6a4514026165d28
Natnael1998 <nati123solo@gmail.com>
2022-09-28 19:03:13 +0300

    invert color

---
+++ index.js
@@ -0,0 +1,31 @@
+/*let user = [{
+    email : "nati",
+    password :"nati123"
+},
+{
+    email : "biruik",
+    password :"biruik123"
+},
+{
+    email : "naol",
+    password :"naol123"
+},
+
+]
+function login(email,password)
+    console.log(user[i])
+    for (i=0;i<user.length;i++){
+        if (user[i].email == email ){
+            if (user[i].password == password ){
+                console.log("logged in")
+            }
+        }
+
+
+        
+    }
+login("nati","nati123")*/
+function toggle (){
+    document.querySelector("body").classList.toggle("dark-theme")
+}
+
